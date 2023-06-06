1306334fc9e28626c3392d59a0c8a9d86de6a2fc
Daniel Kelly <me@danielkelly.io>
2023-06-06 15:49:31 -0500

    1-start

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from "vue";
+const showMenu = ref(false);
+</script>
+
+<template>
+  <button @click="showMenu = !showMenu">
+    {{ showMenu ? "Hide" : "Show" }}
+    Menu
+  </button>
+  <ul v-if="showMenu">
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3</li>
+  </ul>
+</template>
