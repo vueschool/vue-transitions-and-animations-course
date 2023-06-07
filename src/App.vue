bac91d61ef5dcf2c907d8ed11ccf4375f6bc49dc
Daniel Kelly <me@danielkelly.io>
2023-06-07 11:53:33 -0500

    4-nested-transitions-and-explicit-transition-durations

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,74 @@
 <script setup>
 import { ref } from "vue";
-const showMenu = ref(false);
+const showModal = ref(false);
 </script>
 
 <template>
-  <button @click="showMenu = !showMenu">
-    {{ showMenu ? "Hide" : "Show" }}
-    Menu
-  </button>
-  <Transition>
-    <ul v-if="showMenu">
-      <li>Item 1</li>
-      <li>Item 2</li>
-      <li>Item 3</li>
-    </ul>
+  <button @click="showModal = true">Show Modal</button>
+  <Transition :duration="1400">
+    <div v-if="showModal" class="modal-wrapper">
+      <div class="modal-bg" @click="showModal = false"></div>
+      <div class="modal">
+        <button class="modal-close" @click="showModal = false">ｘ</button>
+        <div class="modal-content">hello world</div>
+      </div>
+    </div>
   </Transition>
 </template>
+
 <style>
-/* we will explain what these classes do next! */
-.v-enter-active,
-.v-leave-active {
-  transition: opacity 0.5s ease, transform 0.5s ease;
+.modal-wrapper,
+.modal-bg {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.modal-bg {
+  background: rgba(0, 0, 0, 0.4);
+}
+.modal {
+  position: relative;
+  background: white;
+  padding: 5px;
+  z-index: 2;
+  border-radius: 2px;
+}
+.modal-close {
+  position: absolute;
+  top: 0;
+  right: 1px;
+  font-size: 5px;
+  padding: 0;
+  appearance: none;
+  background: none;
+  border: none;
 }
 
-.v-enter-from {
-  opacity: 0;
-  transform: translateY(-10px);
+.modal,
+.modal-bg {
+  transition: all 1s ease;
 }
 
-.v-leave-to {
+.v-enter-active .modal {
+  transition-delay: 0.4s;
+}
+.v-leave-active .modal-bg {
+  transition-delay: 0.4s;
+}
+
+.v-enter-from .modal-bg,
+.v-leave-to .modal-bg {
   opacity: 0;
-  transform: translateY(10px);
+}
+
+.v-enter-from .modal,
+.v-leave-to .modal {
+  opacity: 0;
+  transform: translateY(-30px);
 }
 </style>
