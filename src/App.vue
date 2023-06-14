4965c7159a20908cae7d2efb21a0919ea413d5fe
Daniel Kelly <me@danielkelly.io>
2023-06-14 09:49:28 -0500

    6-the-transition-appear-prop

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <script setup>
-import Dinner from "@/components/Dinner.vue";
+import { ref } from "vue";
+const show = ref(true);
 </script>
 
 <template>
-  <Dinner />
+  <Transition appear>
+    <div v-if="show">Hello Awesome Dev!</div>
+  </Transition>
+  <div><button @click="show = !show">Toggle</button></div>
 </template>
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
