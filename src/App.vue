61cdf776d887481ac277d8e08f07a02ecfe3fbe2
Daniel Kelly <me@danielkelly.io>
2023-06-14 11:05:43 -0500

    7-named-transitions

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,43 @@ const show = ref(true);
 </script>
 
 <template>
-  <Transition appear>
-    <div v-if="show">Hello Awesome Dev!</div>
+  <Transition name="fade">
+    <span v-if="show">Hello Fade In</span>
   </Transition>
+  <br />
+  <Transition name="pop">
+    <span v-if="show">Hello Pop In</span>
+  </Transition>
+  <br />
   <div><button @click="show = !show">Toggle</button></div>
 </template>
 <style scoped>
-.v-enter-active,
-.v-leave-active {
+/* Fade */
+.fade-enter-active,
+.fade-leave-active {
+  transform-origin: unset;
   transition: opacity 0.5s ease;
 }
 
-.v-enter-from,
-.v-leave-to {
+.fade-enter-from,
+.fade-leave-to {
   opacity: 0;
+}
+
+/* Pop */
+.pop-enter-active,
+.pop-leave-active {
+  transform-origin: center;
+  transition: all 0.5s cubic-bezier(0.175, 0.885, 0.32, 2.275);
+}
+
+.pop-enter-from,
+.pop-leave-to {
+  opacity: 0;
+  transform: scale(1.3);
+}
+
+span {
+  display: inline-block;
 }
 </style>
