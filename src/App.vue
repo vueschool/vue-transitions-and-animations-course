6bff6919748272546ee0dc81abb54743f1ba0c89
Daniel Kelly <me@danielkelly.io>
2023-06-21 12:35:29 -0500

    10-vue-transition-javascript-hooks-and-greensock

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <script setup>
 import { ref } from "vue";
-import FadeTransition from "@/components/FadeTransition.vue";
-import PopTransition from "@/components/PopTransition.vue";
+import GsapTransition from "@/components/GsapTransition.vue";
 const show = ref(true);
 </script>
 
 <template>
-  <FadeTransition>
-    <span v-if="show">Hello Fade In</span>
-  </FadeTransition>
-  <br />
-  <PopTransition>
-    <span v-if="show">Hello Pop In</span>
-  </PopTransition>
-  <br />
   <div><button @click="show = !show">Toggle</button></div>
+  <br />
+  <GsapTransition>
+    <div v-if="show" class="circle"></div>
+  </GsapTransition>
 </template>
+
+<style scoped>
+.circle {
+  @apply w-12 h-12 bg-green-500 rounded-full;
+}
+</style>
---
+++ src/components/GsapTransition.vue
@@ -0,0 +1,44 @@
+<script setup>
+import gsap from "gsap";
+
+function onBeforeEnter(el) {
+  gsap.set(el, {
+    scaleX: 0.25,
+    scaleY: 0.25,
+    opacity: 1,
+  });
+}
+
+function onEnter(el, done) {
+  gsap.to(el, {
+    duration: 1,
+    scaleX: 1,
+    scaleY: 1,
+    opacity: 1,
+    ease: "elastic.inOut(2.5, 1)",
+    onComplete: done,
+  });
+}
+
+function onLeave(el, done) {
+  gsap.to(el, {
+    duration: 1,
+    scaleX: 0,
+    scaleY: 0,
+    opacity: 0.2,
+    ease: "elastic.inOut(2.5, 1)",
+    onComplete: done,
+  });
+}
+</script>
+
+<template>
+  <Transition
+    @before-enter="onBeforeEnter"
+    @enter="onEnter"
+    @leave="onLeave"
+    :css="false"
+  >
+    <slot></slot>
+  </Transition>
+</template>
