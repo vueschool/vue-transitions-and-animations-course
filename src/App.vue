c0055e9e327bc5b3a8059ac63ad74f2cf612a31b
Daniel Kelly <me@danielkelly.io>
2023-06-21 17:06:34 -0500

    13-staggering-list-transitions

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,59 @@
 <script setup>
 import { shuffleArray } from "./helper";
-import { ref } from "vue";
+import { ref, computed } from "vue";
+import gsap from "gsap";
 
 const pokemon = ref([]);
+const search = ref("");
+const matches = computed(() =>
+  pokemon.value.filter((p) =>
+    p.name.toLowerCase().includes(search.value.toLowerCase())
+  )
+);
 
 fetch("https://pokeapi.co/api/v2/pokemon?limit=7").then(async (res) => {
   pokemon.value = (await res.json()).results;
 });
+
+function onBeforeEnter(el) {
+  el.style.opacity = 0;
+  el.style.height = 0;
+}
+
+function onEnter(el, done) {
+  gsap.to(el, {
+    opacity: 1,
+    height: "1.6em",
+    delay: el.dataset.index * 0.1,
+    onComplete: done,
+  });
+}
+
+function onLeave(el, done) {
+  gsap.to(el, {
+    opacity: 0,
+    height: 0,
+    delay: el.dataset.index * 0.1,
+    onComplete: done,
+  });
+}
 </script>
 
 <template>
+  <input type="search" v-model="search" />
   <div v-if="pokemon.length">
-    <TransitionGroup tag="ul">
-      <li v-for="(creature, index) in pokemon" :key="creature.name">
+    <TransitionGroup
+      tag="ul"
+      :css="false"
+      @before-enter="onBeforeEnter"
+      @enter="onEnter"
+      @leave="onLeave"
+    >
+      <li
+        v-for="(creature, index) in matches"
+        :key="creature.name"
+        :data-index="index"
+      >
         {{ creature.name }}
         <button @click="pokemon.splice(index, 1)">X</button>
       </li>
@@ -20,19 +61,3 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=7").then(async (res) => {
     <button @click="shuffleArray(pokemon)">Shuffle</button>
   </div>
 </template>
-
-<style>
-.v-move,
-.v-enter-active,
-.v-leave-active {
-  transition: all 0.5s ease;
-}
-.v-enter-from,
-.v-leave-to {
-  opacity: 0;
-  transform: translateX(30px);
-}
-.v-leave-active {
-  position: absolute;
-}
-</style>
